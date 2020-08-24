c76fc1f57f8d87e521abdef54a3d1db64de5b88f
SonarBoy <Joshua.L99.C@gmail.com>
2020-08-24 14:39:00 -0400

    Databinding in HTML

--- angular-tour-of-heroes/src/app/angular-examples/angular-examples.component.ts
+++ angular-tour-of-heroes/src/app/angular-examples/angular-examples.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-angular-examples',
@@ -15,7 +15,18 @@ export class AngularExamplesComponent implements OnInit {
   recommended = "Test Title"
   itemImageUrl2 = 'assets/kudo.bmp';
 
-
+  /*
+  *  ViewChild
+  * Property decorator that configures a view query. The change detector
+  * looks for the first element or the directive matching the selector in the
+  * view DOM. If the view DOM changes, and a new child matches the selector, the 
+  * property is updated.
+  * 
+  * A template reference variable as a string (e.g. query <my-component #cmp>
+  * </my-component> with @ViewChild('cmp'))
+  */
+  @ViewChild('htmlExample',null) htmlExample;
+  
 
   constructor() { }
 
@@ -29,6 +40,14 @@ export class AngularExamplesComponent implements OnInit {
 
   public breakOut(){
     alert("Break Out!");
+  }
+
+  public getHTMLAttributeValue():any{
+    alert(this.htmlExample.nativeElement.getAttribute('value'));
+  }
+
+  public getDOMPropertyValue():any{
+    alert(this.htmlExample.nativeElement.value);
   }
 
 }
---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+
+
+class SpeechRecognitionApi{
+    constructor(){
+        const speechToText = window.SpeechRecognition || window.webkitSpeechRecognition;
+        this.speechApi = new speechToText();
+        this.output = options.output ? options.output : document.createElement("div");
+        this.speechApi.continuos = true;
+        this.speechApi.interimResult = false;
+        this.speechApi.onresult = (event) => {
+            var resultIndex = event.resultIndex;
+            var transcript = event.results[resultIndex][0].transcript;
+            this.output.textContent = transcript;
+        }
+    }
+
+    init(){
+        this.speechApi.start();
+    }
+
+    stop(){
+        this.speechApi.stop();
+    }
+
+    
+}
+
+window.onload = function(){
+    var speech = new SpeechRecognitionApi({
+        output:document.querySelector(".output")
+    })
+
+    document.querySelector(".btn-start").addEventListener("click",() => {
+        speech.init();
+    });
+    document.querySelector(".btn-end").addEventListener("click",() => {
+        speech.stop();
+    });
+}
